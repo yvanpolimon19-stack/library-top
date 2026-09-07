20616886fda227e8f7ebf4cc894bd3ef7325971d
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2026-09-07 20:56:06 +0400

    added the basic css and html, made the dialog no preventDefault yet

--- javascript.js
+++ javascript.js
@@ -1,22 +1,41 @@
+const container = document.querySelector(".book-list");
+const dialog = document.getElementById("bookDialog");
+const openBtn = document.getElementById("show");
+const closeBtn = document.getElementById("close");
+
+openBtn.addEventListener('click', () => {
+  dialog.showModal(); 
+});
+
+// 2. Close the dialog
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+});
+
+
 const myLibrary = [];
 
 function Book() {
-  constructor(title, author){
+  constructor(title, author, numOfPages);{
     this.title = title;
     this.author = author;
+    this.numOfPages = numOfPages;
+    this.isRead = 0;
     this.id = crypto.randomUUID();
     
     this.displaySelf = function(){
         return `
                 Title : ${this.title}
                 Author: ${this.author}
+                Number of pages: ${this.numOfPages}
+                Read : ${this.isRead}
                 ID    : ${this.id}`;
     }
 
   }
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(book) {
+  
 }
 
